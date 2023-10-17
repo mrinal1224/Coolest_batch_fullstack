c06418722b87e8f879563a29c4a4719511d5fb92
mrinal1224 <iammrinalkc@gmail.com>
2023-10-17 22:27:02 +0530

    Js

---
+++ Class-8(Introduction to JavaScript)/variables.js
@@ -0,0 +1,56 @@
+// // Declare a variable in JavaScript
+
+// var a = 2
+// // Number
+
+// console.log(a)
+// //Dynnamically typed Language
+
+// //String
+// var b = "Scaler is Awesome!!"
+// console.log(b)
+
+
+// var c = true
+// console.log(c)
+
+// var d = null
+
+// console.log(d)
+
+// var e
+// console.log(e)
+
+
+// All about the var keyword
+
+var name1 = 'Narsing'
+
+var name1 = 'Sandipan'
+
+console.log(name1)
+
+
+let name2 = 'Gopi'
+
+name2 = 'Haritha'
+
+console.log(name2)
+
+
+//const 
+
+const a = 40
+
+console.log(a)
+
+
+
+
+
+
+
+
+
+
+
