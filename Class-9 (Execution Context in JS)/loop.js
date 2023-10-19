896d4cf2921db931d68621f62ed9013c10eb18e3
mrinal1224 <iammrinalkc@gmail.com>
2023-10-19 21:52:54 +0530

    loops

---
+++ Class-9 (Execution Context in JS)/loop.js
@@ -0,0 +1,19 @@
+// print the sqaure for all the array members
+
+
+let arr = [1 , 2, 3, 4, 5]
+
+
+let sqauredArr = []
+
+
+// console.log(arr.length)
+
+
+for(let i=0 ; i<arr.length ; i++ ){
+    sqauredArr.push(arr[i]*arr[i])
+}
+
+console.log(sqauredArr)
+
+
