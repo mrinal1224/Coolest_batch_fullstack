588a07744bf06fc7c0fe1a7e546008f245c421b6
mrinal1224 <iammrinalkc@gmail.com>
2023-10-21 21:58:27 +0530

    basic problem

--- Class-10(functional Programming)/callback.js
+++ Class-10(functional Programming)/callback.js
@@ -9,8 +9,9 @@
 
 function printFirstName(firstName , cb1 , cb2){
     console.log(firstName) // Adam
-    cb1('Smith')
-    cb2(32)
+    cb1('Smith') // printlastname
+    cb2(25) // printAge
+
 }
 
 
@@ -26,7 +27,13 @@ function printAge(age){
 
 printFirstName('Adam' , printlastName , printAge)
 
+
+
 // printlastName('Smith')
+
+// printAge(25)
+
+
 
 // Callback Function
 
---
+++ Class-10(functional Programming)/problem.js
@@ -0,0 +1,43 @@
+let radiusArr = [2, 3, 4, 5, 6]; // radius of circles
+
+//Calculate the area for each raidus
+
+function calculateArea(arr) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    result.push(3.14 * arr[i] * arr[i]);
+  }
+
+  return result;
+}
+
+
+function calculatedDiamter(arr) {
+    let diameterResult = [];
+    for (let i = 0; i < arr.length; i++) {
+      diameterResult.push(2 * arr[i]);
+    }
+  
+    return diameterResult;
+  }
+
+  function calculatedCicumference(arr) {
+    let circumferenceResult = [];
+    for (let i = 0; i < arr.length; i++) {
+      circumferenceResult.push(2 * 3.14 * arr[i]);
+    }
+  
+    return circumferenceResult;
+  }
+  
+
+let Areas = calculateArea(radiusArr);
+
+console.log(Areas);
+
+let diameters = calculatedDiamter(radiusArr);
+console.log(diameters);
+
+let circumferneces = calculatedCicumference(radiusArr);
+console.log(circumferneces);
