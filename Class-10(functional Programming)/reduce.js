1f75ac877be8a4132b42eab40c54f0eb963ba78a
mrinal1224 <iammrinalkc@gmail.com>
2023-10-26 21:33:51 +0530

    problems

--- Class-10(functional Programming)/reduce.js
+++ Class-10(functional Programming)/reduce.js
@@ -3,17 +3,17 @@ let numbersArr = [1 , 2, 3, 4 ,5]
 
 // Calculate the sum of alll the array elements
 
-// function sumAll(arr){
-//     let sum = 0
+function sumAll(arr){
+    let sum = 0
 
-//     for(let i=0 ; i<arr.length ; i++){
-//         sum = sum+arr[i]
-//     }
-//    return sum
-// }
+    for(let i=0 ; i<arr.length ; i++){
+        sum = sum+arr[i] 
+    }
+   return sum
+}
 
-// let total = sumAll(numbersArr)
-// console.log(total)
+let total = sumAll(numbersArr)
+console.log(total)
 
 
 
@@ -22,14 +22,14 @@ let sumFromReduce = numbersArr.reduce(function(acc , num){
        return acc
 }, 0)
 
-let productFromReduce = numbersArr.reduce(function(acc , num){
-    acc = acc*num 
-    return acc
-}, 1)
+// let productFromReduce = numbersArr.reduce(function(acc , num){
+//     acc = acc*num 
+//     return acc
+// }, 1)
 
 
 console.log(sumFromReduce)
-console.log(productFromReduce)
+// console.log(productFromReduce)
 
 
 
