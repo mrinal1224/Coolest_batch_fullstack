bec8ab7867b351c25f2081be1e04dae1ceb34d2a
mrinal1224 <iammrinalkc@gmail.com>
2023-11-07 22:42:11 +0530

    push

--- Class-14(Kanban Board)/script.js
+++ Class-14(Kanban Board)/script.js
@@ -2,6 +2,17 @@ let addBtn = document.querySelector(".add-btn");
 let modalCont = document.querySelector(".modal-cont");
 let mainCont = document.querySelector('.main-cont')
 
+let textAreaCont = document.querySelector('.textArea-cont')
+
+let allPriorityColors = document.querySelectorAll('.priority-color')
+
+
+// let colors = ['lightpink' , 'lightblue' , 'lightgreen' , 'black']
+
+let modalPriorityColor = 'lightpink'
+
+console.log(allPriorityColors)
+
 let addTaskFlag = false;
 
 addBtn.addEventListener("click", function () {
@@ -23,21 +34,49 @@ modalCont.addEventListener("keydown", function (e) {
   if (key == "Shift") {
     console.log("ticket created");
 
-       createTicket()
+       createTicket(textAreaCont.value , modalPriorityColor)
   }
 });
 
-function createTicket() {
+
+// Add tasks according to active color
+
+
+allPriorityColors.forEach(function(colorElem){
+   
+    colorElem.addEventListener('click' , function(){
+        allPriorityColors.forEach(function(priorityColorElem){
+            priorityColorElem.classList.remove('active')
+        })
+
+         colorElem.classList.add('active')
+
+         modalPriorityColor = colorElem.classList[0] // lp lb ...
+
+         console.log(modalPriorityColor)
+    })
+})
+
+
+
+
+
+
+
+
+// This function generates a ticket
+
+function createTicket(ticketTask ,  ticketColorClass) {
   let ticketCont = document.createElement("div");
   ticketCont.setAttribute("class", "ticket-cont");
 
-  ticketCont.innerHTML = `<div class="ticket-color"></div>
+  ticketCont.innerHTML = `<div class="ticket-color ${ticketColorClass}"></div>
   <div class="ticket-id">
       1234567
    </div>
 
    <div class="ticket-task">
-      demo task
+      ${ticketTask}
    </div>
 
    <div class="ticket-lock">
