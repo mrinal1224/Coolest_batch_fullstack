ac37c3a73f1b75d9e1f2959837288288c563d2bd
mrinal1224 <iammrinalkc@gmail.com>
2023-12-05 21:53:19 +0530

    push

---
+++ Class-24 (Asynchronous Programming-1)/Sync.js
@@ -0,0 +1,12 @@
+let hello1 = 'Hello 1'
+
+let hello2 = 'Hello 2'
+
+console.log(hello1)
+
+for(let i=0 ; i<10000000000 ; i++){
+
+}
+
+
+console.log(hello2)
---
+++ Class-24 (Asynchronous Programming-1)/async.js
@@ -0,0 +1,14 @@
+let hello3 = "hello3";
+let hello4 = "hello4";
+
+let hello5 = 'hello5'
+
+console.log(hello3);
+
+setTimeout(() => {
+  for (let i = 0; i < 10000000000; i++) {}
+
+  console.log(hello5)
+} , 0);
+
+console.log(hello4);
