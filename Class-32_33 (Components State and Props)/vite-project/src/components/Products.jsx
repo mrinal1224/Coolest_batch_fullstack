f9e93cac45ff65ce503e1c82bd33fa5408402cd7
mrinal1224 <iammrinalkc@gmail.com>
2024-01-06 21:57:42 +0530

    push

--- Class-32_33 (Components State and Props)/vite-project/src/components/Products.jsx
+++ Class-32_33 (Components State and Props)/vite-project/src/components/Products.jsx
@@ -4,10 +4,10 @@ function Products() {
   //  let products = ['Laptops' , 'Headphones' , 'Mouse' , 'Keyboard' , "Laptops"]
 
   let productsObjects = [
-    { name: "Laptop ", price: "35000 ", Brand: "HP" },
+    { name : "Laptop ", price: "35000 ", Brand: "HP" },
     { name: "Headphones ", price: "5000 ", Brand: "Sony" },
     { name: "Mouse ", price: "1000 ", Brand: "HP" },
-    { name: "KeyBoard ", price: '2000 ', Brand: "Dell" },
+    { name: "KeyBoard ", price: "2000 ", Brand: "Dell" },
   ];
   return (
     <div>
