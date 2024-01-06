d59c1205b2a5ec1a745a6d3b626742840bd58c77
mrinal1224 <iammrinalkc@gmail.com>
2024-01-06 21:54:21 +0530

    list with objects

--- Class-32 (Components State and Props)/vite-project/src/components/Products.jsx
+++ Class-32 (Components State and Props)/vite-project/src/components/Products.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Products() {
- let products = ['Laptops' , 'Headphones' , 'Mouse' , 'Keyboard' , "Laptops"]
+  //  let products = ['Laptops' , 'Headphones' , 'Mouse' , 'Keyboard' , "Laptops"]
+
+  let productsObjects = [
+    { name: "Laptop ", price: "35000 ", Brand: "HP" },
+    { name: "Headphones ", price: "5000 ", Brand: "Sony" },
+    { name: "Mouse ", price: "1000 ", Brand: "HP" },
+    { name: "KeyBoard ", price: '2000 ', Brand: "Dell" },
+  ];
   return (
     <div>
-        <ul>
-        {products.map((product , index)=>{
-             return <li key={index}>{index}{product}</li>
+      <ul>
+        {productsObjects.map((product, index) => {
+          return (
+            <li key={index}>
+              Name : {product.name}
+              Price : {product.price}
+              Brand : {product.Brand}
+            </li>
+          );
         })}
-        </ul>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
