3009559466881b6617b4522c9c0d8644f15d7a37
mrinal1224 <iammrinalkc@gmail.com>
2024-01-06 22:45:00 +0530

    forms and lists

--- Class-32_33 (Components State and Props)/vite-project/src/components/Form.jsx
+++ Class-32_33 (Components State and Props)/vite-project/src/components/Form.jsx
@@ -3,22 +3,39 @@ import React , {useState} from 'react'
 function Form() {
 
     const [firstName , setFirstName] = useState('')
+    const [lastName , setLastName] = useState('')
+    
 
-     let handleChange =(e)=>{
+     let handleFirstName =(e)=>{
        setFirstName(e.target.value)
+     }
+
+     let handleLastName =(e)=>{
+        setLastName(e.target.value)
+      }
+
+     let handleSubmit=(e)=>{
+       e.preventDefault()
+       console.log({
+         fname : firstName,
+         lName : lastName
+       })
      }
 
 
   return (
     <div>
-        <h1>This is a Form</h1>
+        <h1 >This is a Form</h1>
 
         <form>
             <label>FirstName </label>
-            <input onChange={handleChange}  type='text' value={firstName}/>
+            <input onChange={handleFirstName}  type='text' value={firstName}/>
+
+            <label>LastName </label>
+            <input onChange={handleLastName}  type='text' value={lastName}/>
        
 
-            <button>Submit</button>
+            <button onClick={handleSubmit}>Submit</button>
         </form>
     </div>
   )
