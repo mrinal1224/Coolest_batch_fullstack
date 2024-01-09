a98fde775f289499a00b7e0ee75e49d3543e6d71
mrinal1224 <iammrinalkc@gmail.com>
2024-01-09 22:26:58 +0530

    push

--- imdb_project/vite-project/src/components/Banner.jsx
+++ imdb_project/vite-project/src/components/Banner.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Banner() {
   return (
-     
-        <div className='h-[20vh] md:h-[75vh] bg-cover bg-center flex items-end' style={{backgroundImage : 'url(https://i.pinimg.com/originals/29/7d/e0/297de0761b0c756266d74ca50d03cc1d.jpg)'}}>
-          <div className='text-white w-full text-center text-2xl'>Avenger Endgame</div>
-        </div>
-
-    
-  )
+    <div
+      className="h-[20vh] md:h-[75vh] bg-cover bg-center flex items-end"
+      style={{
+        backgroundImage:
+          "url(https://i.pinimg.com/originals/29/7d/e0/297de0761b0c756266d74ca50d03cc1d.jpg)",
+      }}
+    >
+      <div className="text-white w-full text-center text-2xl">
+        Avenger Endgame
+      </div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
--- imdb_project/vite-project/src/components/NavBar.jsx
+++ imdb_project/vite-project/src/components/NavBar.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from "react";
 
-import Logo from '../MovieLogo.png'
+import Logo from "../MovieLogo.png";
 
 function NavBar() {
   return (
-    <div className='flex space-x-8 items-center pl-3 py-4'>
-        <img className='w-[50px]' src={Logo}/>
+    <div className="flex space-x-8 items-center pl-3 py-4">
+      <img className="w-[50px]" src={Logo} />
 
-        <a href='/' className='text-blue-500 text-3xl font-bold'>Movies</a>
+      <a href="/" className="text-blue-500 text-3xl font-bold">
+        Movies
+      </a>
 
-        <a href='/watchlist' className='text-blue-500 text-3xl font-bold' >Watchlist</a>
-
-
+      <a href="/watchlist" className="text-blue-500 text-3xl font-bold">
+        Watchlist
+      </a>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
