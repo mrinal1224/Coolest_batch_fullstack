a9bcb81f80d4990224da01762c3052c881b32b08
mrinal1224 <iammrinalkc@gmail.com>
2024-01-11 23:29:46 +0530

    end of day

--- imdb_project/vite-project/src/components/NavBar.jsx
+++ imdb_project/vite-project/src/components/NavBar.jsx
@@ -7,7 +7,7 @@ import {Link} from 'react-router-dom'
 function NavBar() {
   return (
     <div className="flex space-x-8 items-center pl-3 py-4">
-      <img className="w-[50px]" src={Logo} />
+      <Link to='/'><img className="w-[50px]" src={Logo} /></Link>
 
       <Link to="/"  className="text-blue-500 text-3xl font-bold">
         Movies
