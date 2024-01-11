6329b7e73ebee85db82ff6f9cdef71499f10eed1
mrinal1224 <iammrinalkc@gmail.com>
2024-01-11 21:51:34 +0530

    get api data

--- imdb_project/vite-project/src/components/Movies.jsx
+++ imdb_project/vite-project/src/components/Movies.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
-import MovieCard from './MovieCard'
+import React, { useEffect } from "react";
+import MovieCard from "./MovieCard";
+import axios from "axios";
 
 function Movies() {
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/trending/movie/day?api_key=3aec63790d50f3b9fc2efb4c15a8cf99&language=en-US"
+      )
+      .then(function (res) {
+        console.log(res.data.results);
+      });
+  }, []);
+
   return (
     <div>
-        <div className='text-2xl font-bold text-center m-5'>
-        <h1 >Trending Movies</h1>
-        </div>
+      <div className="text-2xl font-bold text-center m-5">
+        <h1>Trending Movies</h1>
+      </div>
 
-        <div className='flex justify-evenly flex-wrap gap-8 '>
-            <MovieCard/>
-            <MovieCard/>
-            <MovieCard/>
-            <MovieCard/>
-            <MovieCard/>
-            <MovieCard/>
-        </div>
+      <div className="flex justify-evenly flex-wrap gap-8 ">
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Movies
+export default Movies;
