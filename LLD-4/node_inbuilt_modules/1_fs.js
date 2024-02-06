026076a79cd72b34f16bc1e669db3c5384fb72c6
mrinal1224 <iammrinalkc@gmail.com>
2024-02-06 22:46:57 +0530

    pushing

--- LLD-4/node_inbuilt_modules/1_fs.js
+++ LLD-4/node_inbuilt_modules/1_fs.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 
 // 1.Read , Write , Update , delete your files
@@ -13,8 +14,8 @@ const fs = require('fs')
 
 // Write a File
 
-fs.writeFileSync('f2.txt' , "This is my Content")
-fs.writeFileSync('f2.txt' , "New data") // writes content to f2
+// fs.writeFileSync('f2.txt' , "This is my Content")
+// fs.writeFileSync('f2.txt' , "New data") // writes content to f2
 
 // fs.writeFileSync('f3.txt' , "this is my content in f3 file")
 
@@ -26,7 +27,61 @@ fs.writeFileSync('f2.txt' , "New data") // writes content to f2
 
 // Delete a file
 
-// fs.unlinkSync('f3.txt')
+//  fs.unlinkSync('f3.txt')
+
+
+ // Fs module with Directories
+
+ //Create and Delete a Directory (folders)
+
+//  fs.mkdirSync('myDirectory1')
+//  fs.mkdirSync('myDirectory2')
+//  fs.mkdirSync('myDirectory3')
+
+//  console.log('Directory Created')
+
+//  fs.rmdirSync('myDirectory2')
+
+//  console.log("directory2 Deleted")
+
+
+ // exitsSync - will help you to check if a file or directory exists or not
+
+
+//  let doesExist = fs.existsSync('myDirectory6')
+
+//  console.log(doesExist)
+
+
+
+ // We will do an Exercise
+
+//  Copy a file from srcFolder to Destinatiom folder
+
+
+let srcFilePath = '/Users/admin/Desktop/Coolest Batch/LLD-4/node_inbuilt_modules/f1.txt'
+
+let destDirectoryPath = '/Users/admin/Desktop/Coolest Batch/LLD-4/node_inbuilt_modules/myDirectory1/'
+
+
+let toBeCopiedFileName = path.basename(srcFilePath)
+
+console.log(toBeCopiedFileName)
+
+const destPath = path.join(destDirectoryPath , toBeCopiedFileName)
+
+console.log(destPath)
+
+
+fs.copyFileSync(srcFilePath , destPath)
+
+fs.unlinkSync(srcFilePath)
+
+ 
+
+
+
+ 
 
 
 
@@ -39,6 +94,10 @@ fs.writeFileSync('f2.txt' , "New data") // writes content to f2
 
 
 
-// Works with Directories
+
+
+
+
+
 
 
---
+++ LLD-4/node_inbuilt_modules/2_path.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+
+// I want to get the extension of a file
+
+// f1.txt , song.mp3 , video.mp4
+
+
+const extension = path.extname('/Users/admin/Desktop/Coolest Batch/LLD-4/node_inbuilt_modules/f1.txt')
+const basename = path.basename('/Users/admin/Desktop/Coolest Batch/LLD-4/node_inbuilt_modules/f1.txt')
+
+console.log(extension)
+console.log(basename)
+
+
+
+
+
+
