8bbff797cef6f578e4d9eab80e621e4474aadf1c
mrinal1224 <iammrinalkc@gmail.com>
2024-02-13 22:56:57 +0530

    push

--- LLD-4/express/db.js
+++ LLD-4/express/db.js
@@ -1,1 +1,44 @@
-const DBurl = 'mongodb+srv://mrinal1224:zCViynGcS65huhOx@cluster0.kxyq8el.mongodb.net/?retryWrites=true&w=majority'
+const mongoose = require("mongoose");
+
+const DBurl =
+  "mongodb+srv://mrinal1224:zCViynGcS65huhOx@cluster0.kxyq8el.mongodb.net/Scaler?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DBurl)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch(() => {
+    console.log("Connection Unsuccesful");
+  });
+
+// How to add Data in our Database
+
+// CourseSchema
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  creator: String,
+  isPublished: Boolean,
+  publishedDate: { type: Date, default: Date.now },
+  Ratings: Number,
+});
+
+// Model
+
+const CourseModel = mongoose.model("CourseModel", courseSchema);
+
+// create a Document (Course entry)
+
+async function createCourse() {
+  const course = new CourseModel({
+    name: "Java",
+    creator: "Mark",
+    isPublished: true,
+    Ratings: 4.7,
+  });
+
+  await course.save();
+}
+
+createCourse();
